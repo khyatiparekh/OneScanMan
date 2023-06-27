820f2a7503a76e07301370ad73d9c30ed189d7d2
Khyati Parekh <khyatijparekh@gmail.com>
2023-06-27 00:37:25 -0700

    Update nmap_scanning.py

--- nmap_scanning.py
+++ nmap_scanning.py
@@ -217,7 +217,7 @@ def run_nmap(ip_address, open_ports, output_dir, colors, service_to_port_map):
 
             nse_scripts = service_to_nse_scripts[service]
             for port in ports:
-                print(f"{colors['yellow']}\r\x1b[K[*] Scanning:{colors['reset']} Port {port}, Scripts {nse_scripts}")
+                print(f"{colors['yellow']}\r\x1b[K[*] Scanning:{colors['reset']} Port {colors['cyan']}{port}{colors['reset']}, Scripts {nse_scripts}")
 
                 future = executor.submit(run_nmap_scripts_on_port, ip_address, port, nse_scripts, colors)
                 futures[future] = (port, nse_scripts)
@@ -233,7 +233,7 @@ def run_nmap(ip_address, open_ports, output_dir, colors, service_to_port_map):
                     f.write(output)
                     f.write("\n\n\n" + "=" * 80 + "\n\n")
 
-            print(f"{colors['green']}\r\x1b[K[\u2713] Completed:{colors['reset']} Port {port}, Scripts {scripts}")
+            print(f"{colors['green']}\r\x1b[K[\u2713] Completed:{colors['reset']} Port {colors['cyan']}{port}{colors['reset']}, Scripts {scripts}")
 
     stop_animation_event.set()
     animation_thread.join()
