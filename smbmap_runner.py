82af9f1c7dd5de334a6623ff366f8b5e3778bf9f
Khyati Parekh <khyatijparekh@gmail.com>
2023-06-29 18:48:20 -0700

    Update smbmap_runner.py

--- smbmap_runner.py
+++ smbmap_runner.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 def run_smbmap(ip_address, output_dir, colors):
-    command = f"sudo smbmap -H {ip_address} > {output_dir}/smbmap_output.txt"
+    command = f"sudo smbmap -H {ip_address} -p Anonymous -u Anonymous > {output_dir}/smbmap_output.txt"
 
     print(f"\n\033[1m{colors['yellow']}[#] Running {colors['cyan']}smbmap{colors['reset']}{colors['reset']}\033[0m [{command}]\n")
 
@@ -23,17 +23,26 @@ def print_smbmap_output(output, colors):
 
 def list_accessible_shares(ip_address, output_dir, smbmap_output, colors):
     accessible_shares = []
+    special_chars = ['\0', '/', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '{', '}', '[', ']', ':', ';', '\'', '<', '>', '?', ',']
+
 
     for line in smbmap_output.splitlines():
         if 'READ' in line or 'WRITE' in line:
             share_name = line.split()[0]
             accessible_shares.append(share_name)
 
+    tmp_share = ""
     for share in accessible_shares:
-        command = f"sudo smbmap -R {share} -H {ip_address} > {output_dir}/{share}_files.txt"
+        tmp_share = share
+        print(tmp_share)
+        for characs in special_chars:
+            if characs in tmp_share:
+                tmp_share = tmp_share.replace(characs, "")
+        command = f"sudo smbmap -R {share} -H {ip_address} -p Anonymous -u Anonymous > {output_dir}/{tmp_share}_files.txt"
+        print(command)
         subprocess.run(command, shell=True, check=True)
 
-        with open(f"{output_dir}/{share}_files.txt", "r") as output_file:
+        with open(f"{output_dir}/{tmp_share}_files.txt", "r") as output_file:
             output = output_file.read()
         print(f"{colors['yellow']}\n[*] Files in {share}:{colors['reset']}")
         for line in output.splitlines():
@@ -49,5 +58,20 @@ def download_files(ip_address, output_dir, share_name, colors):
     os.makedirs(output_folder, exist_ok=True)
 
     command = f"smbclient //{ip_address}/{share_name} -N -c 'prompt OFF; recurse ON; lcd {output_folder}; mget *'"
-    subprocess.run(command, shell=True, check=True)
 
+    try:
+        subprocess.run(command, shell=True, check=True)
+    except:
+        return
+
+
+
+colors = {
+    'red': '\033[91m',
+    'green': '\033[92m',
+    'yellow': '\033[93m',
+    'cyan': '\033[96m',
+    'reset': '\033[0m'
+}
+
+run_smbmap("192.168.186.10", "./Reports/192.168.186.10", colors)
