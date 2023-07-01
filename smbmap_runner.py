c64a9cd395071a53f7f7ecd201b4ef69e8aa5a9d
Khyati Parekh <khyatijparekh@gmail.com>
2023-07-01 00:52:44 -0700

    Update smbmap_runner.py

--- smbmap_runner.py
+++ smbmap_runner.py
@@ -2,9 +2,7 @@ import subprocess
 import os
 
 def run_smbmap(ip_address, output_dir, colors):
-    command = f"sudo smbmap -H {ip_address} -p Anonymous -u Anonymous > {output_dir}/smbmap_output.txt"
-
-    print(f"\n\033[1m{colors['yellow']}[#] Running {colors['cyan']}smbmap{colors['reset']}{colors['reset']}\033[0m [{command}]\n")
+    command = f"sudo smbmap -H {ip_address} > {output_dir}/smbmap_output.txt"
 
     try:
         subprocess.run(command, shell=True, check=True)
@@ -13,18 +11,18 @@ def run_smbmap(ip_address, output_dir, colors):
         print_smbmap_output(output, colors)
         list_accessible_shares(ip_address, output_dir, output, colors)
     except subprocess.CalledProcessError as e:
-        print(f"{colors['red']}\n[-] smbmap failed with error code {e.returncode}\nError message: {e.output}{colors['reset']}")
+        print(f"{colors['red']}[Failure][Samba Discovery][smbmap][{e}]{colors['reset']}")
 
 def print_smbmap_output(output, colors):
-    print(f"{colors['yellow']}[*] smbmap output:{colors['reset']}")
+    print(f"\033[1m{colors['yellow']}[Samba Discovery][{colors['cyan']}smbmap{colors['yellow']}]{colors['reset']}\033[0m")
+
     for line in output.splitlines():
-        if "Working on it..." not in line:
+        if "Working on it..." not in line and "Authentication error" not in line and "445 not open" not in line:
             print(line)
 
 def list_accessible_shares(ip_address, output_dir, smbmap_output, colors):
     accessible_shares = []
     special_chars = ['\0', '/', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '{', '}', '[', ']', ':', ';', '\'', '<', '>', '?', ',']
-
 
     for line in smbmap_output.splitlines():
         if 'READ' in line or 'WRITE' in line:
@@ -34,33 +32,26 @@ def list_accessible_shares(ip_address, output_dir, smbmap_output, colors):
     tmp_share = ""
     for share in accessible_shares:
         tmp_share = share
-        print(tmp_share)
         for characs in special_chars:
             if characs in tmp_share:
                 tmp_share = tmp_share.replace(characs, "")
-        command = f"sudo smbmap -R {share} -H {ip_address} -p Anonymous -u Anonymous > {output_dir}/{tmp_share}_files.txt"
-        print(command)
-        subprocess.run(command, shell=True, check=True)
+        list_all_files(ip_address, share, output_dir, colors)
 
-        with open(f"{output_dir}/{tmp_share}_files.txt", "r") as output_file:
-            output = output_file.read()
-        print(f"{colors['yellow']}\n[*] Files in {share}:{colors['reset']}")
-        for line in output.splitlines():
-            if "Working on it..." not in line:
-                print(line)
+def list_all_files(ip_address, share_name, output_dir, colors):
 
-        download_files(ip_address, output_dir, share, colors)
-
-def download_files(ip_address, output_dir, share_name, colors):
-    print(f"{colors['yellow']}\n[*] Downloading files for share {share_name}:{colors['reset']}")
-
-    output_folder = f"{output_dir}/smbmap_{share_name}-share_downloaded"
+    output_folder = f"{output_dir}/{share_name}_files"
     os.makedirs(output_folder, exist_ok=True)
 
-    command = f"smbclient //{ip_address}/{share_name} -N -c 'prompt OFF; recurse ON; lcd {output_folder}; mget *'"
+    command = f"smbclient //{ip_address}/{share_name} -N -c 'lcd {output_folder}; recurse ON; ls' > {output_dir}/{share_name}_files.txt"
 
     try:
         subprocess.run(command, shell=True, check=True)
-    except:
-        return
+    except subprocess.CalledProcessError as e:
+        print(f"{colors['red']}[Failure][Samba Discovery][smbclient][{e}]{colors['reset']}")
+
+    with open(f"{output_dir}/{share_name}_files.txt", "r") as output_file:
+        output = output_file.read()
+
+    print(f"{colors['yellow']}[{colors['green']}Discovery{colors['yellow']}][Samba Discovery][{colors['cyan']}smbclient{colors['yellow']}]{colors['green']}[{colors['cyan']}List Files: {colors['red']}{share_name}{colors['yellow']}]{colors['reset']}")
+    print(output)
 
