2e34ae327173feea29b7501c928691105abe2d92
Khyati Parekh <khyatijparekh@gmail.com>
2023-07-01 00:51:57 -0700

    Update port_discovery.py

--- port_discovery.py
+++ port_discovery.py
@@ -25,42 +25,60 @@ def parse_nmap_output(output):
 
     return open_ports
 
-def run_nmap(ip_address, output_file, colors, timeout=2000):
-    command = f"sudo nmap -p- -sU -sS --min-rate 1000 --open -T4 {ip_address} -oN {output_file}"
-    print(f"\n\n\033[1m{colors['yellow']}[#] Discovering open Ports: \033[1m{colors['cyan']}nmap{colors['reset']}\033[0m{colors['reset']}\033[0m [{command}]\n")
+def run_nmap(ip_address, output_file, scan_type, colors, timeout=2000):
+    command = [f"sudo nmap -p- -sU --min-rate 1000 --open -T4 {ip_address} -oN {output_file}_udp", 
+                f"sudo nmap -p- -sS --min-rate 1000 --open -T4 {ip_address} -oN {output_file}_tcp",
+                f"sudo nmap -O {ip_address} -oN {output_file}_os"]
+
+    scan_type_u = scan_type.upper()
+    if scan_type == "tcp":
+        command_run = command[1]
+        print(f"\033[1m{colors['yellow']}[Port Discovery][{colors['cyan']}TCP{colors['reset']}\033[1m{colors['yellow']}][{colors['reset']}\033[1m{colors['cyan']}nmap{colors['reset']}\033[1m{colors['yellow']}]{colors['reset']}\033[0m{colors['reset']}\033[0m")
+    elif scan_type == "udp":
+        command_run = command[0]
+        print(f"\033[1m{colors['yellow']}[Port Discovery][{colors['cyan']}UDP{colors['reset']}\033[1m{colors['yellow']}][{colors['reset']}\033[1m{colors['cyan']}nmap{colors['reset']}\033[1m{colors['yellow']}]{colors['reset']}\033[0m{colors['reset']}\033[0m")
+    else:
+        command_run = command[2]
+        print(f"\033[1m{colors['yellow']}[OS Discovery][{colors['cyan']}OS{colors['reset']}\033[1m{colors['yellow']}][{colors['reset']}\033[1m{colors['cyan']}nmap{colors['reset']}\033[1m{colors['yellow']}]{colors['reset']}\033[0m{colors['reset']}\033[0m")
 
     try:
-        output = subprocess.check_output(command, shell=True, text=True, timeout=timeout)
+        output = subprocess.check_output(command_run, shell=True, text=True, timeout=timeout)
         with open(output_file, 'w') as f:
             f.write(output)
         open_ports = parse_nmap_output(output)
         return open_ports
     except subprocess.CalledProcessError as e:
-        print(f"{colors['red']}[-] Nmap port scanning failed with error code {e.returncode}{colors['reset']}")
+        print(f"{colors['red']}[Failure][Port Discovery][{scan_type_u}][nmap][{e}]{colors['reset']}")
         return {}
 
-def run_port_discovery(ip_address, output_file, interface, colors, timeout=2000):
+def run_port_discovery(ip_address, output_file, interface, colors, scan_type, timeout=2000):
 
-    nmap_output_file = output_file.replace("masscan", "nmap")
+    if scan_type == "udp":
+        nmap_output_file = output_file.replace("masscan", "nmap_udp")
+    else:
+        nmap_output_file = output_file.replace("masscan", "nmap_tcp")
+
     if len(nmap_output_file) == 0:
         pass
     else:
-        return run_nmap(ip_address, nmap_output_file, colors)
-
-    print(f"\n\n\033[1m{colors['yellow']}[#] Discovering open Ports: \033[1m{colors['cyan']}masscan{colors['reset']}\033[0m{colors['reset']}\033[0m [{command}]\n")
+        return run_nmap(ip_address, nmap_output_file, scan_type, colors)
 
     command = f"sudo masscan {ip_address} -p1-65535,U:1-65535 --wait 0 --rate 1000 -e {interface} > {output_file}"
+    print(f"\033[1m{colors['yellow']}[Port Discovery]\033[1m{colors['yellow']}[{colors['reset']}\033[1m{colors['cyan']}masscan{colors['reset']}\033[1m{colors['yellow']}]{colors['reset']}\033[0m{colors['reset']}\033[0m")
+
+    if scan_type == "udp":
+        masscan_output_file = output_file.replace("nmap_udp", "masscan_udp")
+    else:
+        masscan_output_file = output_file.replace("nmap_udp", "masscan_tcp")
 
     try:
         subprocess.run(command, shell=True, check=True, timeout=timeout)
-        with open(output_file, 'r') as f:
+        with open(masscan_output_file, 'r') as f:
             output = f.read()
         open_ports = parse_masscan_output(output)
 
         return open_ports
-    
     except subprocess.CalledProcessError as e:
-        print(f"{colors['red']}[-] Masscan failed with error code {e.returncode}. Falling back to nmap for port scanning.{colors['reset']}")
-        nmap_output_file = output_file.replace("masscan", "nmap")
-        return run_nmap(ip_address, nmap_output_file, colors)
+        print(f"{colors['red']}[-][Failure][Port Discovery][masscan][{e}]{colors['reset']}")
+        return False
 
